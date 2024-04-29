466154dc442d5fd78787d4999da221dda21582a1
etherealelement <vitalik.safonov@list.ru>
2024-04-29 12:56:47 +0300

    create: feeatured-icon

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
-import { calc } from "antd/es/theme/internal";
+import { SignInPage } from "@/pages/sign-in";
 
 import { Layout } from "@/layouts/authn";
-
-import { ParagraphComponent, TextField, TitleComponent } from "@/shared/ui";
-import { Input } from "@/shared/ui/Input/Input";
-import { Button } from "@/shared/ui/button";
-import { Logo } from "@/shared/ui/logo";
 
 function App() {
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <SignInPage></SignInPage>
+      </Layout>
     </>
   );
 }
